1d4dcceab66ad34e0c8bd3419e8b02327a5c22af
Stefan Buhrmester <buhrmi@gmail.com>
2024-05-07 03:11:34 +0200

    bit nicer

--- app/frontend/layouts/default.svelte
+++ app/frontend/layouts/default.svelte
@@ -7,19 +7,26 @@
 
 
 <section>
-  <nav>
-    {#if current_user}
-    
-      <div class="flex-1"></div>
-    <!-- <a href="/currencies">Explore</a> -->
-      <button class="btn" on:click={() => router.delete('/session')}>Log out</button>
-    {:else}
-      <div class="flex-1"></div>
-      <a href="/users/new" class="btn primary">Sign up</a>
-      <!-- <div class="flex-1"></div> -->
-    {/if}
-  </nav>
-
+  <div class="md:p-4">
+    <nav>
+      <!-- <img src="$assets/logo.png?h=32" alt="Logo" class="inline-block" /> -->
+      <img src="$assets/textmark.png?h=32" alt="Shitcoin Swap" class="hidden md:block w-full" />
+      {#if current_user}
+      
+        <div class="flex-1"></div>
+      <!-- <a href="/currencies">Explore</a> -->
+        <button class="btn" on:click={() => router.delete('/session')}>Log out</button>
+      {:else}
+        <p class="">
+          Already have an account?<br>
+          <a href="/session/new">Log in</a>
+        </p>
+        <div class="flex-1"></div>
+        <a href="/users/new" class="btn primary">Sign up</a>
+        <!-- <div class="flex-1"></div> -->
+      {/if}
+    </nav>
+  </div>
   <slot />
 
 </section>
@@ -27,12 +34,12 @@
 <style>
   nav {
     position: fixed;
-    bottom: 0.2rem;
-    left: 0.2rem;
-    right: 0.2rem;
+    bottom: 0.7rem;
+    left: 0.7rem;
+    right: 0.7rem;
     /* background: rgb(42, 40, 39); */
     box-shadow: 0 -1px 0 rgba(0, 0, 0, .04), 0 1px 4px rgba(0, 0, 0, .65);
-    padding: 0.2rem;
+    padding: 0.7rem;
     display: flex;
     align-items: center;
     background: #ddd;
@@ -45,14 +52,14 @@
   @media (min-width: 768px) {
     section {
       display: grid;
-      grid-template-columns: 200px 1fr;
+      grid-template-columns: 250px 1fr;
     }
     nav {
       position: sticky;
-      top: 0.4rem;
-      left: 0.4rem;
-      padding: 0.4rem;
-      height: calc(100dvh - 0.8rem);
+      top: 1rem;
+      /* width: 220px; */
+      padding: 1rem;
+      height: calc(100%);
       flex-direction: column;
       .btn {
         width: 100%;
