f697bea8ceeddb4068e6253c7d7d24614c77f32d
Stefan Buhrmester <buhrmi@gmail.com>
2024-05-12 15:25:59 +0200

    no SSR in test

--- app/frontend/entrypoints/browser.js
+++ app/frontend/entrypoints/browser.js
@@ -5,7 +5,7 @@ import './browser.css'
 
 import "toastify-js/src/toastify.css"
 
-import { hydrate } from 'svelte'
+import { hydrate, mount } from 'svelte'
 import { createInertiaApp, router } from '@inertiajs/svelte'
 
 import resolve from './lib/resolve'
@@ -14,7 +14,14 @@ import resolve from './lib/resolve'
 createInertiaApp({ 
   resolve,
   setup({ el, App, props }) {
-    hydrate(App, { target: el, props })
+    // We don't have an SSR server during test
+    if (window.RAILS_ENV == 'test') {
+      mount(App, { target: el, props })
+    }
+    else {
+      hydrate(App, { target: el, props })
+    }
+
   },
 })
 
