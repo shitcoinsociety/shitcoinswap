1c726601578c6afa8271a3b82fefdf6c22d8df9a
Stefan Buhrmester <buhrmi@gmail.com>
2024-05-25 13:09:56 +0000

    light

--- app/frontend/lib/tippy.js
+++ app/frontend/lib/tippy.js
@@ -1,12 +1,12 @@
-// import 'tippy.js/dist/tippy.css'
-import 'tippy.js/themes/material.css'
+import 'tippy.js/dist/tippy.css'
+import 'tippy.js/themes/light.css'
 import tippy from 'tippy.js'
 
 export default function(el, contentOrOptions) {
   const options = typeof(contentOrOptions) == 'string'
     ? {content: contentOrOptions}
     : contentOrOptions
-  options.theme = 'material'
+  options.theme = 'light'
   const instance = tippy(el, options)
   
   return {
