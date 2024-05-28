5f0dad89dee4aa76fbd7f62f2be95a2b78e392d2
Stefan Buhrmester <buhrmi@gmail.com>
2024-05-28 06:29:44 +0000

    fix test lol

--- test/controllers/portfolios_controller_test.rb
+++ test/controllers/portfolios_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class PortfoliosControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get portfolio_url
+    get portfolios_url
     assert_response :redirect
   end
 end
