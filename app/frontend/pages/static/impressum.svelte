7989b120277ed4b8bc574a070f1b8ed18b2f746a
Stefan Buhrmester <buhrmi@gmail.com>
2024-06-17 21:30:00 +0000

    impressum

--- app/frontend/pages/static/impressum.svelte
+++ app/frontend/pages/static/impressum.svelte
@@ -5,7 +5,7 @@
   </h1>
   <div class="p-4 md:p-8">
 <p>
-  Authorized representative and responsible within the meaning of Section 5 TMG is<br>
+  Authorized representative and responsible within the meaning of § 5 TMG is<br>
   Stefan Buhrmester<br>
   Schlossstr. 29<br>
   64625 Bensheim<br>
@@ -16,6 +16,14 @@
   Phone: <a href="tel:+49 176 82083555">+49 176 82083555</a><br>
   Email: <a href="mailto:support@shitcoinswap.com">support@shitcoinswap.com</a>
 </p>
+
+<p class="mt-4">
+  VAT-Identification Number:<br> DE 337477941
+</p>
+<p class="mt-4 mb-8">
+  Platform for online dispute resolution by the EU-Commission:<br> <a href="https://ec.europa.eu/consumers/odr" target="_blank">https://ec.europa.eu/consumers/odr</a>
+</p>
+
 </div>
 <img src="~/assets/onboarding/rave.webp" alt="" class="w-full rounded-2">
 </div>
