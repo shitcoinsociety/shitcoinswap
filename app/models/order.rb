3849960a1b2379beaa3b29374c052197b6097994
Stefan Buhrmester <buhrmi@gmail.com>
2024-06-19 03:17:33 +0200

    validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
   belongs_to :have_project, class_name: "Project", foreign_key: "have_symbol", primary_key: "symbol", optional: true
   belongs_to :want_project, class_name: "Project", foreign_key: "want_symbol", primary_key: "symbol", optional: true
 
-  before_validate :set_price
+  before_validation :set_price
 
   validates :have_symbol, :want_symbol, :have_amount, :want_amount, presence: true
   validates :price, numericality: { greater_than: 0, greater_than_or_equal_to: 0.00000001 }
