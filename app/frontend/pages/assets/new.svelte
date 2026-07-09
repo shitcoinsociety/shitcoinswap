fa61f834ec6c1ff40fa82f727163192eb3637807
Stefan Buhrmester <stefan@buhrmi.de>
2026-07-10 01:17:24 +0700

    we ask

--- app/frontend/pages/assets/new.svelte
+++ app/frontend/pages/assets/new.svelte
@@ -8,10 +8,10 @@
   </section>
   <section>
     <p>
-      Thank you for your interest in tokenizing with the Shitcoin Society.
+      Thank you for your interest in listing your startup with the Shitcoin Society.
     </p>
     <p>
-      We ask founders who want to list with us to join us for a public founder interview video on our <a href="https://www.youtube.com/@shitcoinsociety">YouTube channel</a>.
+      At this time, we ask all interested founders to join us for a public interview video on our <a href="https://www.youtube.com/@shitcoinsociety">YouTube channel</a>.
     </p>
     <p>
       Or check back later when our automated listing process is available.
