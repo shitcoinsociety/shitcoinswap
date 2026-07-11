a8c1b962c6bdd4cb653cfeacb0559fc61d744916
Stefan Buhrmester <stefan@buhrmi.de>
2026-07-11 13:42:33 +0700

    social links

--- app/frontend/layouts/default.svelte
+++ app/frontend/layouts/default.svelte
@@ -26,29 +26,28 @@
 
 <footer>
   <section class="container">
-    <div class="flex flex-col gap-4 md:flex-row justify-between w-full">
-      <div>
-        <img src={"/logo_white.svg"} class="h-10" alt="Shitcoin Society" />
+    <div class="flex flex-col items-center sm:items-start">
+      <img src={"/logo_white.svg"} class="h-10" alt="Shitcoin Society" />
+      <span>
         Make something nobody asked for.
-      </div>
-      <div>
-        <h4>Resources</h4>
-        <ul>
-          <li>
-            <a
-              href="https://github.com/shitcoinsociety"
-              target="_blank"
-              rel="noopener noreferrer">GitHub</a
-            >
-          </li>
-        </ul>
-      </div>
+      </span>
     </div>
   </section>
-  <section class="container border-t border-t-white/20 !mt-8 pt-8 text-sm">
+  <section class="container border-t border-t-white/20 !mt-8 pt-8 text-sm flex justify-between items-center flex-col sm:flex-row gap-4">
     <div>
-     <p>&copy; 2026 Shitcoin Society</p>
-   </div>
+      &copy; 2026 Shitcoin Society
+    </div>
+    <div class="flex gap-4">
+      <a href="https://x.com/shitcoinswap" target="_blank" rel="noopener noreferrer">
+        <div class="text-xl i-bi:twitter-x">X</div>
+      </a>
+      <a href="https://www.youtube.com/@shitcoinsociety" target="_blank" rel="noopener noreferrer">
+        <div class="text-xl i-bi:youtube">YouTube</div>
+      </a>
+      <a href="https://github.com/shitcoinsociety" target="_blank" rel="noopener noreferrer">
+        <div class="text-xl i-bi:github">GitHub</div>
+      </a>
+    </div>
 
   </section>
 </footer>
